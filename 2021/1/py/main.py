93ba61d7855d914845e15df7817afc404cd82499
Max H <maxplaysbest@gmail.com>
2022-09-21 18:03:05 +1200

    Started on script

--- 2021/1/py/main.py
+++ 2021/1/py/main.py
@@ -1,6 +1,17 @@
 import os
 print("Started")
 with open("input.txt", 'r') as f:
-    read = f.readline
-    print(read)
-print("Script")
+    inputfile = f.readline()
+    for i in inputfile:
+        dmi = 0
+        previous = i
+        if previous == 0:
+            continue
+        if previous == 9299:
+            break
+        else:
+            if i > previous:
+                dmi = dmi + 1
+            else:
+                continue
+    print(dmi)
